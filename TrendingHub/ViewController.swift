dfe6f8527eb1b1ac11965c885c190e67c75bd53b
Yuri Kobets <yuri.kobets1@gmail.com>
2020-09-26 10:58:13 +0300

    added project

---
+++ TrendingHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrendingHub
+//
+//  Created by Yurii Kobets on 9/21/20.
+//  Copyright © 2020 Helloworld Association. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
