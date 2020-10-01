9160643e29e399477c4275179fdb3eedcffeefac
Yurii Kobets <yuri.kobets1@gmail.com>
2020-10-01 19:18:40 +0300

    Implemented search with UISearchBar

--- TrendingHub/Presentation/RepositryList/ViewModel/RepositoryListViewModel.swift
+++ TrendingHub/Presentation/RepositryList/ViewModel/RepositoryListViewModel.swift
@@ -12,6 +12,7 @@ import RxRelay
 protocol RepositoryListViewModelInput {
     func viewWillAppear()
     func didSelect(item: RepositoryViewModel)
+    func startSearch(searchTerm: String?)
 }
 
 protocol RepositoryListViewModel : RepositoryListViewModelInput {
@@ -19,18 +20,21 @@ protocol RepositoryListViewModel : RepositoryListViewModelInput {
     var itemsRelay: BehaviorRelay<[RepositoryViewModel]> { get }
 }
 
+typealias RepositoryListViewDidSelectAction = (Repository) -> Void
+
 final class RepositoryListViewModelImpl : RepositoryListViewModel {
     
     let itemsRelay = BehaviorRelay<[RepositoryViewModel]>(value: [])
-    
-    private var cachedItems: [RepositoryViewModel]?
+    private var cachedItems = [Repository]()
     
     let title = "Github Trends"
     
     private let fetchUseCase : FetchRepositoriesUseCase
+    private let didSelectAction : RepositoryListViewDidSelectAction
     
-    init(fetchUseCase : FetchRepositoriesUseCase) {
+    init(fetchUseCase : FetchRepositoriesUseCase, didSelect : @escaping RepositoryListViewDidSelectAction) {
         self.fetchUseCase = fetchUseCase
+        didSelectAction = didSelect
     }
 }
 
@@ -39,15 +43,33 @@ extension RepositoryListViewModelImpl {
         
         _ = fetchUseCase.fetch { [weak self] (result) in
             guard let self = self else { return }
+            
             switch result {
-            case .success(let repositories):   
-                self.itemsRelay.accept(repositories.map { RepositoryViewModel(repository: $0) })
+            case .success(let repositories):
+                self.cachedItems = repositories
+                self.itemsRelay.accept(repositories.map { RepositoryViewModelImpl(repository: $0) })
             case .failure: break
             }
         }
     }
     
     func didSelect(item: RepositoryViewModel) {
+        if let selectedRepository = cachedItems.filter({ $0.name == item.projectName }).first {
+            didSelectAction(selectedRepository)
+        }
+    }
+    
+    func startSearch(searchTerm: String?) {
+        guard let searchTerm = searchTerm, !searchTerm.isEmpty else {
+            self.itemsRelay.accept(cachedItems.map { RepositoryViewModelImpl(repository: $0) })
+            return
+        }
         
+        let term = searchTerm.lowercased()
+        
+        let filteredResult = cachedItems
+            .filter({ $0.name.lowercased().contains(term) || $0.description.lowercased().contains(term)})
+            .map({ RepositoryViewModelImpl(repository: $0) })
+        self.itemsRelay.accept(filteredResult)
     }
 }
